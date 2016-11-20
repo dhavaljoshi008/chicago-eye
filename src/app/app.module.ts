1cc614bcf4094e26100d28c4f235371cbd62f4c2
dhavaljoshi008 <dhavaljoshi008@gmail.com>
2016-11-19 22:15:30 -0600

    add dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpModule } from '@angular/http';
 import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, DashboardComponent
   ],
   imports: [
     BrowserModule,
