b110c1e08f3d781bb72b08f5fdbefa025d048d45
dhavaljoshi008 <dhavaljoshi008@gmail.com>
2016-11-19 21:42:41 -0600

    add app title and menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { MenuItem } from './menu-item';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  appTitle: string;
+  menuItems: MenuItem[];
+  
+  constructor() {
+    this.appTitle = 'Chicago Eye';
+    this.menuItems = [ { caption: 'Dashboard', link: '#' },
+                     ];
+  }
 }
